20c4ab244090afc3a3f3c40ff5b218a0f47e605f
Felix Basinger <basingerfelix17@gmail.com>
2024-03-14 18:55:19 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+if (!e.target.matches('.dropbtn')) {
+var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+    myDropdown.classList.remove('show');
+    }
+}
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var myVideo = documeent.getElementById("myVideo");
+    if (myVideo) {
+        myVideo.muted = true;
+        myVideo.play();
+    }
+});
